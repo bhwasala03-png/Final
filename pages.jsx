8a4b52472aa842cdbf40ba461e2a281d46a4cd8c
Binara Heshan Wasala <bhwasala03@gmail.com>
2026-03-25 00:50:36 +0530

    Add Login component with form and button

---
+++ pages.jsx
@@ -0,0 +1,14 @@
+import Button from "../components/Button";
+
+export default function Login() {
+  return (
+    <div className="login-page">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" className="input-field" />
+        <input type="password" placeholder="Password" className="input-field" />
+        <Button text="Login" />
+      </form>
+    </div>
+  );
+}
